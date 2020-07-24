49e548cef42e5245ac4b31e37934e14c666b15cc
むずでょ <muzudho1@gmail.com>
2020-07-25 06:09:16 +0900

    scanner

--- src/syntax_parser.rs
+++ src/syntax_parser.rs
@@ -3,17 +3,38 @@ use crate::lexical_parser::{TokenLine, TokenType};
 use casual_logger::{Log, Table};
 
 #[derive(Debug)]
-enum MachineState {
+enum LineSyntaxParserMachineState {
     First,
     /// `key = right_value`.
     KeyPairSyntax,
     Unimplemented,
 }
-/// `key = right_value`.
-#[derive(Debug)]
-enum KeyPairSyntaxMachineState {
-    AfterKey,
-    AfterEquals,
+
+pub struct LineSyntaxScanner {}
+impl Default for LineSyntaxScanner {
+    fn default() -> Self {
+        LineSyntaxScanner {}
+    }
+}
+impl LineSyntaxScanner {
+    pub fn scan_line(&mut self, token_line: &TokenLine) {
+        let mut line_syntax_parser = LineSyntaxParser::default();
+
+        for token in &token_line.tokens {
+            line_syntax_parser.parse_token(token_line, token);
+        }
+        // End of line.
+
+        if let Some(key_value_syntax) = &line_syntax_parser.key_value_syntax {
+            Log::info_t(
+                "LineSyntaxParser#parse_line/AfterEndOfLine",
+                Table::default().str("key", &key_value_syntax.key).str(
+                    "right_value",
+                    &format!("{:?}", key_value_syntax.right_value_buf),
+                ),
+            );
+        }
+    }
 }
 
 pub struct LineSyntaxParser {
@@ -27,113 +48,120 @@ impl Default for LineSyntaxParser {
     }
 }
 impl LineSyntaxParser {
-    pub fn parse_line(&mut self, token_line: &TokenLine) {
-        let mut state: MachineState = MachineState::First;
-        let mut key_pair_state: KeyPairSyntaxMachineState = KeyPairSyntaxMachineState::AfterKey;
+    pub fn parse_token(&mut self, token_line: &TokenLine, token: &Token) {
+        let mut state: LineSyntaxParserMachineState = LineSyntaxParserMachineState::First;
 
-        for token in &token_line.tokens {
-            match state {
-                MachineState::First => match token.type_ {
-                    TokenType::Key => {
-                        Log::info_t(
-                            "",
-                            Table::default()
-                                .str("token_line", &format!("{:?}", token_line))
-                                .str("state", &format!("{:?}", state))
-                                .str("key_pair_state", &format!("{:?}", key_pair_state))
-                                .str("token", &format!("{:?}", token)),
-                        );
-                        self.key_value_syntax = Some(KeyValueSyntaxParser::new(&token.value));
-                        state = MachineState::KeyPairSyntax;
-                    }
-                    _ => {
-                        state = MachineState::Unimplemented;
-                    }
-                },
-                MachineState::KeyPairSyntax => match key_pair_state {
-                    KeyPairSyntaxMachineState::AfterKey => {
-                        match token.type_ {
-                            TokenType::WhiteSpace => {} //Ignored it.
-                            TokenType::Equals => {
-                                key_pair_state = KeyPairSyntaxMachineState::AfterEquals;
-                            }
-                            _ => panic!(Log::fatal_t(
-                                "",
-                                Table::default()
-                                    .str("token_line", &format!("{:?}", token_line))
-                                    .str("state", &format!("{:?}", state))
-                                    .str("key_pair_state", &format!("{:?}", key_pair_state))
-                                    .str("token", &format!("{:?}", token))
-                            )),
-                        }
-                    }
-                    KeyPairSyntaxMachineState::AfterEquals => {
-                        if let Some(key_value_syntax) = &mut self.key_value_syntax {
-                            key_value_syntax.parse(token);
-                        } else {
-                            panic!(Log::fatal_t(
-                                "",
-                                Table::default()
-                                    .str("token_line", &format!("{:?}", token_line))
-                                    .str("state", &format!("{:?}", state))
-                                    .str("key_pair_state", &format!("{:?}", key_pair_state))
-                                    .str("token", &format!("{:?}", token))
-                            ));
-                        }
-                    }
-                },
-                MachineState::Unimplemented => {
+        match state {
+            LineSyntaxParserMachineState::First => match token.type_ {
+                TokenType::Key => {
                     Log::info_t(
                         "",
                         Table::default()
                             .str("token_line", &format!("{:?}", token_line))
                             .str("state", &format!("{:?}", state))
-                            .str("key_pair_state", &format!("{:?}", key_pair_state))
                             .str("token", &format!("{:?}", token)),
                     );
+                    self.key_value_syntax = Some(KeyValueSyntaxParser::new(&token.value));
+                    state = LineSyntaxParserMachineState::KeyPairSyntax;
                 }
-            }
-        }
-        // End of line.
-
-        match state {
-            MachineState::KeyPairSyntax => {
-                if let Some(key_value_syntax) = &self.key_value_syntax {
-                    Log::info_t(
-                        "LineSyntaxParser#parse_line/AfterEndOfLine",
-                        Table::default().str("key", &key_value_syntax.key).str(
-                            "right_value",
-                            &format!("{:?}", key_value_syntax.right_value_buf),
-                        ),
-                    );
+                _ => {
+                    state = LineSyntaxParserMachineState::Unimplemented;
+                }
+            },
+            LineSyntaxParserMachineState::KeyPairSyntax => {
+                if let Some(key_value_syntax) = &mut self.key_value_syntax {
+                    key_value_syntax.parse(token_line, token);
                 } else {
                     panic!(Log::fatal_t(
                         "",
                         Table::default()
                             .str("token_line", &format!("{:?}", token_line))
                             .str("state", &format!("{:?}", state))
-                            .str("key_pair_state", &format!("{:?}", key_pair_state))
+                            .str("token", &format!("{:?}", token))
                     ));
                 }
             }
-            _ => {}
+            LineSyntaxParserMachineState::Unimplemented => {
+                Log::info_t(
+                    "",
+                    Table::default()
+                        .str("token_line", &format!("{:?}", token_line))
+                        .str("state", &format!("{:?}", state))
+                        .str("token", &format!("{:?}", token)),
+                );
+            }
         }
     }
 }
 
+/// `key = right_value`.
+#[derive(Debug)]
+enum KeyValueSyntaxMachineState {
+    AfterKey,
+    AfterEquals,
+    // AfterLeftCurlyBracket,
+}
+
 /// `key = value`.
 pub struct KeyValueSyntaxParser {
+    key_pair_state: KeyValueSyntaxMachineState,
     key: String,
     right_value_buf: TokenLine,
 }
 impl KeyValueSyntaxParser {
     pub fn new(key: &str) -> Self {
         KeyValueSyntaxParser {
+            key_pair_state: KeyValueSyntaxMachineState::AfterKey,
             key: key.to_string(),
             right_value_buf: TokenLine::default(),
         }
     }
-    pub fn parse(&mut self, token: &Token) {
-        self.right_value_buf.tokens.push(token.clone());
+    pub fn parse(&mut self, token_line: &TokenLine, token: &Token) {
+        match self.key_pair_state {
+            KeyValueSyntaxMachineState::AfterKey => {
+                match token.type_ {
+                    TokenType::WhiteSpace => {} //Ignored it.
+                    TokenType::Equals => {
+                        self.key_pair_state = KeyValueSyntaxMachineState::AfterEquals;
+                    }
+                    _ => panic!(Log::fatal_t(
+                        "",
+                        Table::default()
+                            .str("token_line", &format!("{:?}", token_line))
+                            .str("state", &format!("{:?}", self.key_pair_state))
+                            .str("key_pair_state", &format!("{:?}", self.key_pair_state))
+                            .str("token", &format!("{:?}", token))
+                    )),
+                }
+            }
+            KeyValueSyntaxMachineState::AfterEquals => {
+                // key_value_syntax.parse(token_line, token);
+                match token.type_ {
+                    TokenType::LeftCurlyBracket => {}
+                    _ => {
+                        self.right_value_buf.tokens.push(token.clone());
+                    }
+                }
+            }
+        }
     }
+}
+
+/*
+/// `{ key = value, key = value }`.
+#[derive(Debug)]
+enum InlineTableSyntaxMachineState {
+    AfterLeftCurlyBracket,
+}
+*/
+
+/// `{ key = value, key = value }`.
+pub struct InlineTableSyntaxParser {}
+impl Default for InlineTableSyntaxParser {
+    fn default() -> Self {
+        InlineTableSyntaxParser {}
+    }
+}
+impl InlineTableSyntaxParser {
+    pub fn parse(&mut self, token_line: &TokenLine, token: &Token) {}
 }
