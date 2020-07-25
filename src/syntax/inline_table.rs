a84c9a85d27fca6215727160a7adb7d54b8019d9
むずでょ <muzudho1@gmail.com>
2020-07-25 10:20:39 +0900

    再帰をどう struct で表現しよ☆（＾～＾）

--- src/syntax/inline_table.rs
+++ src/syntax/inline_table.rs
@@ -1,29 +1,40 @@
-use crate::lexical_parser::Token;
-use crate::lexical_parser::TokenLine;
+use crate::lexical_parser::{Token, TokenLine, TokenType};
+// use crate::syntax::key_value::KeyValueSyntaxParser;
 use casual_logger::Table;
 
-/*
 /// `{ key = value, key = value }`.
 #[derive(Debug)]
 enum InlineTableSyntaxMachineState {
     AfterLeftCurlyBracket,
 }
-*/
 
 /// `{ key = value, key = value }`.
 pub struct InlineTableSyntaxParser {
+    state: InlineTableSyntaxMachineState,
     contents: TokenLine,
+    //key_value_syntax_parser: Option<KeyValueSyntaxParser>,
 }
 impl Default for InlineTableSyntaxParser {
     fn default() -> Self {
         InlineTableSyntaxParser {
+            state: InlineTableSyntaxMachineState::AfterLeftCurlyBracket,
             contents: TokenLine::default(),
+            //key_value_syntax_parser: None,
         }
     }
 }
 impl InlineTableSyntaxParser {
     pub fn parse(&mut self, token_line: &TokenLine, token: &Token) {
-        self.contents.tokens.push(token.clone());
+        match self.state {
+            InlineTableSyntaxMachineState::AfterLeftCurlyBracket => {
+                match token.type_ {
+                    TokenType::WhiteSpace => {} // Ignore it.
+                    TokenType::Key => {}
+                    _ => {}
+                }
+                self.contents.tokens.push(token.clone());
+            }
+        }
     }
     pub fn log(&self) -> Table {
         Table::default()
