15f67d265087e25a250a42fabf64fb01f5c7ec2d
むずでょ <muzudho1@gmail.com>
2020-10-18 00:03:09 +0900

    lexical parser

--- src/parser/phase100/lexical_parser.rs
+++ src/parser/phase100/lexical_parser.rs
@@ -10,7 +10,7 @@ enum State {
     Numerals,
     /// Whitespace means tab ('\t' 0x09) or space (' ' 0x20).  
     /// ホワイトスペースは タブ ('\t', 0x09) と 半角スペース (' ' 0x20) です。  
-    WhiteSpace,
+    WhiteSpaces,
 }
 
 /// Lexical parser.  
@@ -45,7 +45,7 @@ impl LexicalParser {
     }
     pub fn parse_line(&mut self, line: &str) {
         let ch_vec: Vec<char> = line.chars().collect();
-        let mut initial_column_number = 0;
+        let mut column_number_of_token = 0;
         let mut j = 0;
         let mut chars: (Option<&char>, Option<&char>) = (None, None);
         for (i, ch) in ch_vec.iter().enumerate() {
@@ -56,7 +56,7 @@ impl LexicalParser {
             chars.0 = chars.1;
             chars.1 = Some(ch);
             if let Some(_) = chars.0 {
-                self.one_delay_loop(i - 1, chars, &mut initial_column_number);
+                self.one_delay_loop(i - 1, chars, &mut column_number_of_token);
             }
             j = i;
         }
@@ -66,11 +66,11 @@ impl LexicalParser {
         chars.0 = chars.1;
         chars.1 = None;
         if let Some(_) = chars.0 {
-            self.one_delay_loop(j, chars, &mut initial_column_number);
+            self.one_delay_loop(j, chars, &mut column_number_of_token);
         }
 
         // Log::info("End of line.");
-        self.flush(initial_column_number);
+        self.flush(column_number_of_token);
         // Append an end of line.
         // 行末を追加します。
         self.product.tokens.push(Token::new(
@@ -84,135 +84,155 @@ impl LexicalParser {
         &mut self,
         i: usize,
         chars: (Option<&char>, Option<&char>),
-        initial_column_number: &mut usize,
+        column_number_of_token: &mut usize,
     ) {
         let ch0 = chars.0.unwrap();
         let column_number = i + 1;
         match self.state {
             State::Alphabets => {
-                match ch0 {
-                    'A'..='Z' | 'a'..='z' => {
-                        // Alphabet.
-                        self.buf.push(*ch0);
-                    }
-                    _ => {
-                        self.initial(*initial_column_number, *ch0);
-                        *initial_column_number = column_number;
+                self.flush(column_number);
+                self.buf.push(*ch0);
+                if let Some(ch1) = chars.1 {
+                    match ch1 {
+                        'A'..='Z' | 'a'..='z' => {}
+                        _ => {
+                            self.state = State::First;
+                        }
                     }
                 }
             }
             State::First => {
-                self.initial(*initial_column_number, *ch0);
-                *initial_column_number = column_number;
+                self.flush(column_number);
+                self.buf.push(*ch0);
+                *column_number_of_token = column_number;
+                match ch0 {
+                    // A ～ Z, a ～ z.
+                    'A'..='Z' | 'a'..='z' => {
+                        self.buf_token_type = TokenType::AlphabetString;
+                        if let Some(ch1) = chars.1 {
+                            match ch1 {
+                                'A'..='Z' | 'a'..='z' => {
+                                    self.state = State::Alphabets;
+                                }
+                                _ => {
+                                    self.state = State::First;
+                                }
+                            }
+                        }
+                    }
+                    // \
+                    '\\' => {
+                        self.buf_token_type = TokenType::Backslash;
+                    }
+                    // :
+                    ':' => {
+                        self.buf_token_type = TokenType::Colon;
+                    }
+                    // ,
+                    ',' => {
+                        self.buf_token_type = TokenType::Comma;
+                    }
+                    // .
+                    '.' => {
+                        self.buf_token_type = TokenType::Dot;
+                    }
+                    // "
+                    '"' => {
+                        self.buf_token_type = TokenType::DoubleQuotation;
+                    }
+                    // =
+                    '=' => {
+                        self.buf_token_type = TokenType::Equals;
+                    }
+                    // -
+                    '-' => {
+                        self.buf_token_type = TokenType::Hyphen;
+                    }
+                    // {
+                    '{' => {
+                        self.buf_token_type = TokenType::LeftCurlyBracket;
+                    }
+                    // [
+                    '[' => {
+                        self.buf_token_type = TokenType::LeftSquareBracket;
+                    }
+                    '0'..='9' => {
+                        self.buf_token_type = TokenType::NumeralString;
+                        if let Some(ch1) = chars.1 {
+                            match ch1 {
+                                '0'..='9' => {
+                                    self.state = State::Numerals;
+                                }
+                                _ => {
+                                    self.state = State::First;
+                                }
+                            }
+                        }
+                    }
+                    // +
+                    '+' => {
+                        self.buf_token_type = TokenType::Plus;
+                    }
+                    // }
+                    '}' => {
+                        self.buf_token_type = TokenType::RightCurlyBracket;
+                    }
+                    // ]
+                    ']' => {
+                        self.buf_token_type = TokenType::RightSquareBracket;
+                    }
+                    // #
+                    '#' => {
+                        self.buf_token_type = TokenType::Sharp;
+                    }
+                    // '
+                    '\'' => {
+                        self.buf_token_type = TokenType::SingleQuotation;
+                    }
+                    // _
+                    '_' => {
+                        self.buf_token_type = TokenType::Underscore;
+                    }
+                    // Whitespace.
+                    '\t' | ' ' => {
+                        self.buf_token_type = TokenType::WhiteSpace;
+                        if let Some(ch1) = chars.1 {
+                            match ch1 {
+                                '\t' | ' ' => {
+                                    self.state = State::WhiteSpaces;
+                                }
+                                _ => {
+                                    self.state = State::First;
+                                }
+                            }
+                        }
+                    }
+                    _ => {
+                        self.buf_token_type = TokenType::Unknown;
+                    }
+                }
             }
             State::Numerals => {
-                match ch0 {
-                    '0'..='9' => {
-                        // Numeral.
-                        self.buf.push(*ch0);
-                    }
-                    _ => {
-                        self.initial(*initial_column_number, *ch0);
-                        *initial_column_number = column_number;
-                    }
-                }
-            }
-            State::WhiteSpace => {
-                // 最初に出てくる文字まで飛ばします。
-                match ch0 {
-                    '\t' | ' ' => {
-                        self.buf.push(*ch0);
-                    }
-                    _ => {
-                        self.initial(*initial_column_number, *ch0);
-                        *initial_column_number = column_number;
+                self.buf.push(*ch0);
+                if let Some(ch1) = chars.1 {
+                    match ch1 {
+                        '0'..='9' => {}
+                        _ => {
+                            self.state = State::First;
+                        }
                     }
                 }
             }
-        }
-    }
-    /// Character at first.  
-    /// 最初の文字。  
-    fn initial(&mut self, column_number: usize, ch: char) {
-        self.flush(column_number);
-        self.buf.push(ch);
-        match ch {
-            // A ～ Z, a ～ z.
-            'A'..='Z' | 'a'..='z' => {
-                self.buf_token_type = TokenType::AlphabetString;
-                self.state = State::Alphabets;
-            }
-            // \
-            '\\' => {
-                self.buf_token_type = TokenType::Backslash;
-            }
-            // :
-            ':' => {
-                self.buf_token_type = TokenType::Colon;
-            }
-            // ,
-            ',' => {
-                self.buf_token_type = TokenType::Comma;
-            }
-            // .
-            '.' => {
-                self.buf_token_type = TokenType::Dot;
-            }
-            // "
-            '"' => {
-                self.buf_token_type = TokenType::DoubleQuotation;
-            }
-            // =
-            '=' => {
-                self.buf_token_type = TokenType::Equals;
-            }
-            // -
-            '-' => {
-                self.buf_token_type = TokenType::Hyphen;
-            }
-            // {
-            '{' => {
-                self.buf_token_type = TokenType::LeftCurlyBracket;
-            }
-            // [
-            '[' => {
-                self.buf_token_type = TokenType::LeftSquareBracket;
-            }
-            '0'..='9' => {
-                self.buf_token_type = TokenType::NumeralString;
-                self.state = State::Numerals;
-            }
-            // +
-            '+' => {
-                self.buf_token_type = TokenType::Plus;
-            }
-            // }
-            '}' => {
-                self.buf_token_type = TokenType::RightCurlyBracket;
-            }
-            // ]
-            ']' => {
-                self.buf_token_type = TokenType::RightSquareBracket;
-            }
-            // #
-            '#' => {
-                self.buf_token_type = TokenType::Sharp;
-            }
-            // '
-            '\'' => {
-                self.buf_token_type = TokenType::SingleQuotation;
-            }
-            // _
-            '_' => {
-                self.buf_token_type = TokenType::Underscore;
-            }
-            // Whitespace.
-            '\t' | ' ' => {
-                self.buf_token_type = TokenType::WhiteSpace;
-                self.state = State::WhiteSpace;
-            }
-            _ => {
-                self.buf_token_type = TokenType::Unknown;
+            State::WhiteSpaces => {
+                self.buf.push(*ch0);
+                if let Some(ch1) = chars.1 {
+                    match ch1 {
+                        '\t' | ' ' => {}
+                        _ => {
+                            self.state = State::First;
+                        }
+                    }
+                }
             }
         }
     }
