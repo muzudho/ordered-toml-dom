fa03b35028f5e6245cd897a259b194b385b6ece2
むずでょ <muzudho1@gmail.com>
2020-10-18 19:10:05 +0900

    debug

--- src/parser/phase200/layer210/escape_sequence_p.rs
+++ src/parser/phase200/layer210/escape_sequence_p.rs
@@ -7,6 +7,7 @@ use crate::parser::phase200::{
     layer210::{EscapeSequenceP, PResult},
 };
 use casual_logger::Table;
+use std::char::from_u32;
 
 /// Syntax machine state.  
 /// 構文状態遷移。  
@@ -19,6 +20,8 @@ pub enum State {
     // After `\`.
     // `\` の後。
     EscapedCharacter,
+    Unicode4digit,
+    Unicode8digit,
 }
 
 impl Default for EscapeSequenceP {
@@ -82,23 +85,30 @@ impl EscapeSequenceP {
                     TokenType::AlphabetCharacter => {
                         // TODO 汎用的に書けないか？
                         // https://doc.rust-lang.org/reference/tokens.html
-                        let code = match token0.to_string().as_str() {
-                            "n" => "\n",
-                            "r" => "\r",
-                            "t" => "\t",
-                            "u" => panic!("Unimplemented unicode 0000."), // TODO
-                            "U" => panic!("Unimplemented unicode 00000000."), // TODO
+                        let mut code = None;
+                        match token0.to_string().as_str() {
+                            "n" => code = Some("\n"),
+                            "r" => code = Some("\r"),
+                            "t" => code = Some("\t"),
+                            "u" => {
+                                self.state = State::Unicode4digit;
+                            }
+                            "U" => {
+                                self.state = State::Unicode8digit;
+                            }
                             _ => {
                                 return error(&mut self.log(), tokens, "escape_sequence_p.rs.206.")
                             }
-                        };
-                        self.buffer.push(Token::new(
-                            token0.column_number,
-                            code,
-                            TokenType::AlphabetCharacter, // TODO EscapeSequence
-                        ));
-                        self.state = State::End;
-                        return PResult::End;
+                        }
+                        if let Some(code) = code {
+                            self.buffer.push(Token::new(
+                                token0.column_number,
+                                code,
+                                TokenType::AlphabetCharacter, // TODO EscapeSequence
+                            ));
+                            self.state = State::End;
+                            return PResult::End;
+                        }
                     }
                     TokenType::Backslash => {
                         self.buffer.push(Token::new(
@@ -124,6 +134,62 @@ impl EscapeSequenceP {
                     }
                 }
             }
+            State::Unicode4digit => match token0.type_ {
+                TokenType::NumeralString => {
+                    let s = token0.to_string();
+                    let (s1, s2) = if 4 < s.len() {
+                        (s[0..4].to_string(), s[4..].to_string())
+                    } else {
+                        (s[0..].to_string(), "".to_string())
+                    };
+
+                    self.buffer.push(Token::new(
+                        token0.column_number,
+                        &from_u32(s1.parse().unwrap()).unwrap().to_string(),
+                        TokenType::AlphabetCharacter, // TODO EscapeSequence
+                    ));
+                    if 0 < s2.len() {
+                        self.buffer.push(Token::new(
+                            token0.column_number + 4, // TODO だいたい4つうしろ？
+                            &s2.to_string(),
+                            TokenType::AlphabetCharacter, // TODO EscapeSequence
+                        ));
+                    }
+                    self.state = State::End;
+                    return PResult::End;
+                }
+                _ => {
+                    return error(&mut self.log(), tokens, "escape_sequence_p.rs.139.");
+                }
+            },
+            State::Unicode8digit => match token0.type_ {
+                TokenType::NumeralString => {
+                    let s = token0.to_string();
+                    let (s1, s2) = if 8 < s.len() {
+                        (s[0..8].to_string(), s[8..].to_string())
+                    } else {
+                        (s[0..].to_string(), "".to_string())
+                    };
+
+                    self.buffer.push(Token::new(
+                        token0.column_number,
+                        &from_u32(s1.parse().unwrap()).unwrap().to_string(),
+                        TokenType::AlphabetCharacter, // TODO EscapeSequence
+                    ));
+                    if 0 < s2.len() {
+                        self.buffer.push(Token::new(
+                            token0.column_number + 8, // TODO だいたい8つうしろ？
+                            &s2.to_string(),
+                            TokenType::AlphabetCharacter, // TODO EscapeSequence
+                        ));
+                    }
+                    self.state = State::End;
+                    return PResult::End;
+                }
+                _ => {
+                    return error(&mut self.log(), tokens, "escape_sequence_p.rs.139.");
+                }
+            },
         }
 
         PResult::Ongoing
