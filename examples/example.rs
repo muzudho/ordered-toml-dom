23dc6db67df436601a0acd6e27d00e247efcf946
むずでょ <muzudho1@gmail.com>
2020-10-23 22:01:41 +0900

    DateTime

--- examples/example.rs
+++ examples/example.rs
@@ -6,6 +6,10 @@
 extern crate tomboy_toml_dom;
 
 use chrono::prelude::{DateTime, Utc};
+use chrono::FixedOffset;
+use chrono::NaiveDate;
+use chrono::NaiveDateTime;
+use chrono::NaiveTime;
 use tomboy_toml_dom::Toml;
 
 fn main() {
@@ -182,5 +186,68 @@ is preserved.
                 .parse::<DateTime<Utc>>()
                 .unwrap()
         )
+    );
+
+    assert_eq!(
+        doc.get_datetime_utc_by_key("odt1"),
+        Some("1979-05-27T07:32:00Z".parse::<DateTime<Utc>>().unwrap())
+    );
+
+    assert_eq!(
+        doc.get_datetime_fixed_offset_by_key("odt2"),
+        Some(
+            "1979-05-27T00:32:00-07:00"
+                .parse::<DateTime<FixedOffset>>()
+                .unwrap()
+        )
+    );
+
+    assert_eq!(
+        doc.get_datetime_fixed_offset_by_key("odt3"),
+        Some(
+            "1979-05-27T00:32:00.999999-07:00"
+                .parse::<DateTime<FixedOffset>>()
+                .unwrap()
+        )
+    );
+
+    // TODO Local datetime
+    assert_eq!(
+        // "1979-05-27T07:32:00". Toml の独自書式か。該当するフォーマット定義見つからず。
+        doc.get_naive_datetime_by_key("ldt1"),
+        Some(
+            match NaiveDateTime::parse_from_str("1979-05-27T07:32:00", "%Y-%m-%dT%H:%M:%S") {
+                Ok(n) => n,
+                Err(why) => panic!("{}", why),
+            }
+        )
+    );
+
+    assert_eq!(
+        // "1979-05-27T00:32:00.999999".
+        doc.get_naive_datetime_by_key("ldt2"),
+        Some(
+            NaiveDateTime::parse_from_str("1979-05-27T00:32:00.999999", "%Y-%m-%dT%H:%M:%S%.6f")
+                .unwrap()
+        )
+    );
+
+    assert_eq!(
+        // "1979-05-27".
+        doc.get_naive_date_by_key("ld1"),
+        Some(match NaiveDate::parse_from_str("1979-05-27", "%Y-%m-%d") {
+            Ok(n) => n,
+            Err(why) => panic!("{}", why),
+        })
+    );
+
+    assert_eq!(
+        doc.get_naive_time_by_key("lt1"),
+        Some(NaiveTime::parse_from_str("07:32:00", "%H:%M:%S").unwrap())
+    );
+
+    assert_eq!(
+        doc.get_naive_time_by_key("lt2"),
+        Some(NaiveTime::parse_from_str("00:32:00.999999", "%H:%M:%S%.6f").unwrap())
     );
 }
