36a5e5f8d1626cd035403abf8e9cd6094db44732
むずでょ <muzudho1@gmail.com>
2020-11-01 20:26:49 +0900

    WIP

--- src/model/layer110/mod.rs
+++ src/model/layer110/mod.rs
@@ -78,6 +78,7 @@ pub enum TokenType {
     Comment,
     DateTime,
     EscapeSequence,
+    Table,
     /// White space.
     WS,
 }
--- src/parser/phase200/layer210/header_p_of_array_of_table.rs
+++ src/parser/phase200/layer210/header_p_of_array_of_table.rs
@@ -1,9 +1,13 @@
 //! Array of HeaderOfArrayOfTable syntax parser.  
 //! テーブルの配列構文パーサー。  
 
-use crate::model::{layer110::TokenType, layer230::HeaderOfArrayOfTable};
+use crate::model::{
+    layer110::{CharacterType, TokenType},
+    layer230::HeaderOfArrayOfTable,
+};
 use crate::parser::phase200::layer210::{HeaderPOfArrayOfTable, PResult};
 use crate::parser::phase200::LookAheadCharacters;
+use crate::parser::phase200::Token;
 // use casual_logger::Table;
 
 impl HeaderPOfArrayOfTable {
@@ -19,23 +23,23 @@ impl HeaderPOfArrayOfTable {
     }
     /// # Arguments
     ///
-    /// * `tokens` - Tokens contains look ahead.  
+    /// * `characters` - Tokens contains look ahead.  
     ///             先読みを含むトークン。  
     /// # Returns
     ///
     /// * `PResult` - Result.  
     ///                             結果。
-    pub fn parse(&mut self, tokens: &LookAheadCharacters) -> PResult {
-        let token0 = tokens.current.as_ref().unwrap();
-        match token0.type_ {
-            TokenType::DoubleQuotation => {
+    pub fn parse(&mut self, characters: &LookAheadCharacters) -> PResult {
+        let character0 = characters.current.as_ref().unwrap();
+        match character0.type_ {
+            CharacterType::DoubleQuotation => {
                 // End of syntax.
                 // 構文の終わり。
                 return PResult::End;
             }
             _ => {
                 let m = self.buffer.as_mut().unwrap();
-                m.push_token(&token0);
+                m.push_token(&Token::from_character(&character0, TokenType::Table));
             }
         }
         PResult::Ongoing
